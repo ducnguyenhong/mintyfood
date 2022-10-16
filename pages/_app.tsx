179f24dd48f0257f3e7fbad85c7d8cd24ea5109c
Nguyen Duc <autoclickvn@gmail.com>
2022-10-16 22:09:08 +0700

    Init project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+
+config.autoAddCss = false
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ChakraProvider><Component {...pageProps} /></ChakraProvider>
 }
 
 export default MyApp
